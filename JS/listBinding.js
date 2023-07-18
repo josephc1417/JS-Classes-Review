07b3d38c04d6d94aba116745634edb37687b27a2
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-07-18 18:02:25 -0400

    Added update method to update the HTML doc with new list items

--- JS/listBinding.js
+++ JS/listBinding.js
@@ -6,16 +6,34 @@
       
       //this instance property (listElement) refers to the physical HTML ELEMENT
       this.listElement = element;
-      this.textList = [];
+      this.textList = [
+         "hi","bye","Go"
+      ];
     }
 
-     //This instance method will update the DOM with the items Contained in //*>> this.textList[] <<*/
+    // creating a static method (helper method )
+    // create an item <li> with text passed into it
+    // item <li> + text = [] >> appended to this.listElement = ul
+    static createListElement(text) {
+         const li = document.createElement("li");
+         li.textContent = text;
+
+         return li;
+      
+    }
+
+
+    //This instance method will update the DOM with the items Contained in //*>> this.textList[] <<*/
      updates() {
       //* Remove all existing <li> element tags first from parent Element(this.listElement)
       while (this.listElement.firstChild){
            this.listElement.removeChild(this.listElement.firstChild);
       }
-
-
+      
+      //this will loop through the array of sting literals and append each array element to this.listElement
+      // we are appending the stat
+      for (const text of this.textList) {
+         this.listElement.appendChild(ListBinding.createListElement(text));
      }
- }
+   }
+}
